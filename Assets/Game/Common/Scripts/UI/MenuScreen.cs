a21078756a4c85ac6393b3550a37ab687f4b4e96
parthosengupta <partho.s.sengupta@gmail.com>
2018-01-17 17:51:35 +0530

    Menu works now.
    Made menu functional

--- Assets/Game/Common/Scripts/UI/MenuScreen.cs
+++ Assets/Game/Common/Scripts/UI/MenuScreen.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using com.nidb.data;
 
 namespace com.nidb.ecigame.ui
@@ -15,6 +16,8 @@ namespace com.nidb.ecigame.ui
 
 		[SerializeField]
 		private Button _menuButtonTemplate;
+		[SerializeField]
+		private float _yOffset;
 
 		[SerializeField]
 		private RectTransform _menuContentRoot;
@@ -22,13 +25,26 @@ namespace com.nidb.ecigame.ui
 		// Use this for initialization
 		void Start () 
 		{
-			
+			RefreshMenu();
 		}
 		
-		// Update is called once per frame
-		void Update () 
+		private void RefreshMenu()
 		{
-			
+			float btnX = 8;
+			float btnY = -150;
+			for(int i = 0; i < _episodesDatabase.pEpisodeCount; ++i)
+			{
+				GameObject btnObj = (GameObject)GameObject.Instantiate(_menuButtonTemplate.gameObject);
+				RectTransform btnTrans = btnObj.GetComponent<RectTransform>();
+				btnTrans.SetParent(_menuContentRoot);
+				btnTrans.anchoredPosition = new Vector2(btnX, btnY);
+				btnTrans.localScale = new Vector3(1, 1, 1);
+				btnX += btnTrans.sizeDelta.x;
+				_episodesDatabase.SetupEpisodeButton(btnObj.GetComponent<Button>(), i);
+				btnObj.SetActive(true);
+				int epIdx = i;
+				btnObj.GetComponent<Button>().onClick.AddListener( () => _episodesDatabase.LoadEpisode(epIdx));
+			}
 		}
 	}
 }
--- Assets/Game/Minigames/Data/ECIGameEpisodes.cs
+++ Assets/Game/Minigames/Data/ECIGameEpisodes.cs
@@ -1,8 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEditor;
+using UnityEngine.UI;
 
 namespace com.nidb.data
 {
@@ -16,9 +17,17 @@ namespace com.nidb.data
 		[Tooltip("The starting scene for the episode")]
 		[SerializeField]
 		private string _sceneName;
+		[Tooltip("Icon used to denote the Episode")]
+		[SerializeField]
+		private Sprite _icon;
+		[Tooltip("Episode Description")]
+		[SerializeField]
+		private string _description;
 
 		public string pName { get { return _name; } }
 		public string pSceneName { get { return _sceneName; } }
+		public Sprite pIcon { get { return _icon; } }
+		public string pDescription { get { return _description; } }
 	}
 
 	public class ECIGameEpisodes : ScriptableObject 
@@ -26,6 +35,8 @@ namespace com.nidb.data
 		[Tooltip("Lists all active episodes in the game")]
 		[SerializeField]
 		private ECIGameEpisodeInfo[] _episodes;
+
+		public int pEpisodeCount { get { return _episodes.Length; } }
 
 		[MenuItem("NIDB/ECI Game Tools/Data/Create Game Episodes List")]
 		public static void CreateAsset()
@@ -44,6 +55,16 @@ namespace com.nidb.data
 		{
 			if(episodeIndex >= 0 && episodeIndex < _episodes.Length)
 				SceneManager.LoadScene(_episodes[episodeIndex].pSceneName);
+		}
+
+		public void SetupEpisodeButton(Button episodeButton, int episodeIndex)
+		{
+			if(episodeIndex >= 0 && episodeIndex < _episodes.Length)
+			{
+				ECIGameEpisodeInfo epInfo = _episodes[episodeIndex];
+				episodeButton.image.sprite = epInfo.pIcon;
+				episodeButton.GetComponentInChildren<Text>().text = epInfo.pName;
+			}
 		}
 	}
 }
