236d70a0cb934ff57d830739e68dd02dabd16285
parthosengupta <partho.s.sengupta@gmail.com>
2018-01-17 16:14:15 +0530

    UI Flow scenes and Episode 1
    Loading scene (Fake loading)
    Menu Scene (WiP)
    Episode 1 (WiP)
    Added Fungus commands for Say dialog with text response.

---
+++ Assets/Game/Common/Scripts/UI/MenuScreen.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using com.nidb.data;
+
+namespace com.nidb.ecigame.ui
+{
+	/**
+	 * Implements the Episodes Menu screen
+	 */
+	public class MenuScreen : MonoBehaviour 
+	{
+		[SerializeField]
+		private ECIGameEpisodes _episodesDatabase;
+
+		[SerializeField]
+		private Button _menuButtonTemplate;
+
+		[SerializeField]
+		private RectTransform _menuContentRoot;
+
+		// Use this for initialization
+		void Start () 
+		{
+			
+		}
+		
+		// Update is called once per frame
+		void Update () 
+		{
+			
+		}
+	}
+}
---
+++ Assets/Game/Minigames/Data/ECIGameEpisodes.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEditor;
+
+namespace com.nidb.data
+{
+	[System.Serializable]
+	public class ECIGameEpisodeInfo
+	{
+		[Tooltip("The name of the episode")]
+		[SerializeField]
+		private string _name;
+
+		[Tooltip("The starting scene for the episode")]
+		[SerializeField]
+		private string _sceneName;
+
+		public string pName { get { return _name; } }
+		public string pSceneName { get { return _sceneName; } }
+	}
+
+	public class ECIGameEpisodes : ScriptableObject 
+	{
+		[Tooltip("Lists all active episodes in the game")]
+		[SerializeField]
+		private ECIGameEpisodeInfo[] _episodes;
+
+		[MenuItem("NIDB/ECI Game Tools/Data/Create Game Episodes List")]
+		public static void CreateAsset()
+		{
+			ECIGameEpisodes obj = ScriptableObject.CreateInstance<ECIGameEpisodes>();
+			string path = AssetDatabase.GetAssetPath(Selection.activeObject);
+			if(string.IsNullOrEmpty(path))
+				path = "Assets";
+			string assetPathAndName = AssetDatabase.GenerateUniqueAssetPath(path + "/" + typeof(ECIGameEpisodes).ToString() + ".asset");
+			AssetDatabase.CreateAsset(obj, assetPathAndName);
+			AssetDatabase.SaveAssets();
+			AssetDatabase.Refresh();
+		}
+
+		public void LoadEpisode(int episodeIndex)
+		{
+			if(episodeIndex >= 0 && episodeIndex < _episodes.Length)
+				SceneManager.LoadScene(_episodes[episodeIndex].pSceneName);
+		}
+	}
+}
